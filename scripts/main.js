70c11b5382715b6da885ff4ab911f67b3d041a89
Illia Horbenko <hrufustwrps@gmail.com>
2024-06-03 15:48:47 +0200

    Changes to be committed: 	modified:   index.html 	modified:   scripts/main.js 	modified:   styles/style.css

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,18 @@
 function toggleFilter(filterType) {
-	let buttons = document.querySelectorAll(".filter");
+	let buttons = document.querySelectorAll('.filter')
 
 	buttons.forEach(function (button) {
 		if (button.classList.contains(filterType)) {
-			button.id = "active";
+			button.id = 'active'
 		} else {
-			button.removeAttribute("id");
+			button.removeAttribute('id')
 		}
-	});
+	})
 }
 
-document.querySelector(".menu").addEventListener("click", function () {
-	document.querySelector(".menu").classList.toggle("menu-active");
+document.querySelector('.menu').addEventListener('click', function () {
+	document.querySelector('.menu').classList.toggle('menu-active')
 	document
-		.querySelector(".navigation__menu")
-		.classList.toggle("navigation-active");
-});
+		.querySelector('.navigation__menu')
+		.classList.toggle('navigation-active')
+})
